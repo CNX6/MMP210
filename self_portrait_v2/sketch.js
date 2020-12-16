897e58dbed523b206ab4f391b3a6f7432610d101
CNX6 <71354447+CNX6@users.noreply.github.com>
2020-12-16 14:09:34 -0500

    Project 2

--- self_portrait_v2/sketch.js
+++ self_portrait_v2/sketch.js
@@ -3,6 +3,20 @@
    by Carl Charleston
 */
 
+// global scope
+var x = 260; // character origin
+var y = 100;
+
+var leftEyeX = 230; // statement
+var rightEyeX = 300;
+
+var eyeSize = 100;
+var mouthSize = eyeSize;
+
+var faceRed = 205;
+var faceGreen = 123;
+var faceBlue = 26;
+
 function setup() {
 	createCanvas(600, 300);
 }
@@ -10,27 +24,35 @@ function setup() {
 function draw() {
 	background( 'red');
 
+ 	x = mouseX;
+ 	y = mouseY;
+
 	//hair
 	fill(0,0,0);
 	noStroke();
-	circle(260, 100, 260);
-
-
+	circle(x, y, 260);
+    
 	//face
-	fill(205, 123, 26);
-	circle(260, 200, 150);
+	fill(205, 123, 26); 
+	circle(x, y + 100, 150);
+
 
 	//eye
 	fill(254, 253, 253); // left
-	circle(230, 200, 30);
+	circle(x - 30, y + 100, 30);
+    
 	fill (254, 253, 253);
-	circle (300, 200, 30); // right
+	circle (x + 40, y + 100, 30); // right
 
 	//mouth
 	stroke("INDIANRED");
 	strokeWeight(10);
 	noFill();
-	ellipse(270, 240, 60, 30);
-	
+	ellipse(x + 10, y + 140, 60, 30);
 
+
+    // visualize origin
+    stroke ('pink');
+	circle (x, y, 10);
+	
 }
